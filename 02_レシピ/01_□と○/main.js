ba1da8d96519399f5eeeefd9299b1ea1ba0ee785
ryuma <ryuma001204@gmail.com>
2024-11-16 09:37:26 +0900

    [ストップウォッチ]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,17 +1,22 @@
-const squareButton = document.getElementById("square-button")
-const circleButton = document.getElementById("circle-button")
-
 const figure = document.getElementById("figure")
+// figure.addEventListener("click", function () {
+//   if (figure.classList.contains("triangled")) {
+//     figure.classList.remove("triangled")
+//   } else if (figure.classList.contains("rounded")) {
+//     figure.classList.add("triangled")
+//     figure.classList.remove("rounded")
+//   } else {
+//     figure.classList.add("rounded")
+//   }
+// })
 
 figure.onclick = function () {
-  figure.classList.toggle("rounded")
-}
-circleButton.onclick = function () {
-  // figure に rounded クラスを追加する
-  figure.classList.add("rounded")
-}
-
-squareButton.onclick = function () {
-  // figure から rounded クラスを削除する
-  figure.classList.remove("rounded")
+  if (figure.classList.contains("triangled")) {
+    figure.classList.remove("triangled")
+  } else if (figure.classList.contains("rounded")) {
+    figure.classList.add("triangled")
+    figure.classList.remove("rounded")
+  } else {
+    figure.classList.add("rounded")
+  }
 }
--- 02_レシピ/01_カウンター/電卓2/main.js
+++ 02_レシピ/01_カウンター/電卓2/main.js
@@ -7,6 +7,9 @@ const equalButton = document.getElementById("equal-button")
 
 const keypoint = document.getElementById("key.")
 const keyC = document.getElementById("keyC")
+let calculate_type = NaN
+let result = ""
+let middle_result = 0
 for (let i = 0; i <= 9; i++) {
   var button = document.getElementById(`key${i}`)
 
@@ -15,38 +18,27 @@ for (let i = 0; i <= 9; i++) {
     display.textContent = result
   }
 }
-let calculate_type = NaN
-let result = ""
-let middle_result = 0
+
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function () {
-  // result を更新
   middle_result = result
   result = ""
   calculate_type = "plus"
-  // display.textContent = ""
-  // result を表示
 }
 minusButton.onclick = function () {
-  // result を更新
   middle_result = result
   result = ""
   calculate_type = "minus"
-  // result を表示
 }
 crossButton.onclick = function () {
-  // result を更新
   middle_result = result
   result = ""
   calculate_type = "cross"
-  // display.textContent = 0
 }
 divideButton.onclick = function () {
-  // result を更新
   middle_result = result
   result = ""
   calculate_type = "divide"
-  // display.textContent = 0
 }
 equalButton.onclick = function () {
   // result を更新
@@ -59,8 +51,9 @@ equalButton.onclick = function () {
   } else if (calculate_type === "divide") {
     result = Number(middle_result) / Number(result)
   }
-
+  result = Math.round(result * 10 ** 8) / 10 ** 8
   calculate_type = NaN
+
   // result を表示
   display.textContent = result
 }
